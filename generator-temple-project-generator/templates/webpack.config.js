07bf4daa62402c040251a5a7e3b893063c66ff7e
temple-deng <630435132@qq.com>
2016-06-29 15:32:09 +0800

    first commit

---
+++ generator-temple-project-generator/templates/webpack.config.js
@@ -0,0 +1,40 @@
+/**
+ * webpack配置文件，注意这个文件实际上没有配置信息， 而是根据环境选择加载对应的配置文件
+ */
+
+
+'use strict';
+
+const path = require('path');
+
+const args = process.env.NODE_ENV;
+
+// 支持两种环境 development: 开发环境 production: 线上环境
+var allowedEnvs = ['development', 'production'];
+
+
+// 设置环境
+var env;
+if (args.env) {
+  env = args.env;
+} else {
+  env = 'development';     // 默认是dev环境
+}
+process.env.REACT_WEBPACK_ENV = env;
+
+
+/**
+ * 根据环境加载配置文件
+ * @param  {String} 环境参数
+ * @return {Object} Webpack config 配置文件
+ */
+function buildConfig(wantedEnv) {
+  var isValid = wantedEnv && wantedEnv.length > 0 && allowedEnvs.indexOf(wantedEnv) !== -1;
+  var validEnv = isValid ? wantedEnv : 'development';
+  var config = require(path.join(__dirname, 'webpack.' + validEnv === 'development' ? 'dev' : 'dist'  + '.config'));
+  return config;
+}
+
+module.exports = buildConfig(env);
+
+
---
+++ generator-temple-project-generator/templates/webpack.dev.config.js
@@ -0,0 +1,37 @@
+/**
+ * 开发环境的配置文件
+ */
+
+'use strict';
+
+let path = require('path');
+let webpack = require('webpack');
+let baseConfig = require('./webpack.base.config');
+let srcPath = path.join(__dirname, '/src');
+
+let config = Object.assign({}, baseConfig, {
+  entry: {
+    'simple-counter': [
+      'webpack-dev-server/client?http://localhost:8080',
+      'webpack/hot/dev-server',
+      './src/index'
+    ]
+  },
+  cache: true,
+  devtool: 'eval-source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+});
+
+//config.module.preLoaders = [
+//                              {
+//                                test: /\.(js|jsx)$/,
+//                                include: srcPath,
+//                                loader: 'eslint-loader'
+//                              }
+//                            ];
+
+
+module.exports = config;
